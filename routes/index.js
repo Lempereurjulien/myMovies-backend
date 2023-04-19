e812d22fd1f5425f7fe7351736dd3847e616e90a
Lempereur <lempereur.julien83@gmail.com>
2023-04-19 14:32:46 +0200

    s

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const MyApi = process.env.TMDB_API_KEY;
+// const MyApi = process.env.TMDB_API_KEY;
+const MyApi = '4e0cecae944829c8da40abccbf45dafe';
 router.get('/movies', (req, res) => {
-fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${MyApi}`)
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=4e0cecae944829c8da40abccbf45dafe`)
 .then(response => response.json())
 .then(data => res.json({movies :data.results}));
 })
